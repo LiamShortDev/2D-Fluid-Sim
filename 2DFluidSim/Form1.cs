bfcb20658bc1b9f14beba5f0fa50d2fbbede386a
LiamShortDev <liamshort.workplace@gmail.com>
2025-08-07 11:59:37 +0100

    First commit

--- 2DFluidSim/Form1.cs
+++ 2DFluidSim/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,132 @@ namespace _2DFluidSim
 {
     public partial class Form1 : Form
     {
+        public struct Vector
+        {
+            public double x;
+            public double y;
+            public Vector(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        PictureBox pictureBox1 = new PictureBox();
+        Bitmap bitmap;
+        int width = 400;
+        int height = 400;
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.D1)
+            {
+                _dyeInjecting = false;
+                _waterInteracting = true;
+            } 
+            if(e.KeyCode == Keys.D2)
+            {
+
+                _dyeInjecting = true;
+                _waterInteracting = false;
+            }
+        }
         public Form1()
         {
+            this.KeyPreview = true;
+            this.KeyDown += MainForm_KeyDown;
             InitializeComponent();
+            InitializeSimulationDisplay();
+            StartSimulationLoop();
+        }
+        private bool _dyeInjecting = true;
+        private bool _waterInteracting = false;
+        private bool _mousedown = false;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mousedown = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _mousedown = false;
+        }
+        public void InitializeSimulationDisplay()
+        {
+            pictureBox1.Size = new Size(width, height);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+            pictureBox1.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+            this.Controls.Add(pictureBox1);
+
+            bitmap = new Bitmap(width, height);
+            pictureBox1.Image = bitmap;
+            Program.InitialiseVelocities();
+            Program.InitialiseDye();
+        }
+
+        public void StartSimulationLoop()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 32; // ~60 FPS
+            timer.Tick += (s, e) => UpdateDisplay();
+            timer.Start();
+        }
+
+        public void UpdateDisplay()
+        {
+            if (_mousedown)
+            {
+                var pos = pictureBox1.PointToClient(Cursor.Position);
+                if (_dyeInjecting)
+                {
+                    Program.InjectDye(pos.X, pos.Y);
+                }
+                if (_waterInteracting)
+                {
+                    Program.ApplyMouseAcceleration(pos.X, pos.Y);
+                }
+                Program.mousePos1 = Program.mousePos2;
+                Program.mousePos2 = new Program.Vector(pos.X, pos.Y);
+            }
+            BitmapData data = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.WriteOnly,
+                PixelFormat.Format32bppArgb
+            );
+            Program.Vector[,] velocities = Program.GetVelocities();
+            Color[,] dye = Program.GetDye();
+
+            unsafe
+            {
+                byte* scan0 = (byte*)data.Scan0;
+                int stride = data.Stride;
+
+                double maxVelocity = 50.0; // set a reasonable upper bound
+
+                for (int y = 0; y < 400; y++)
+                {
+                    byte* row = scan0 + y * stride;
+                    for (int x = 0; x < 400; x++)
+                    {
+                        /* velocity visualisation
+                        Program.Vector v = velocities[x, y];
+                        double magnitude = Math.Sqrt(v.x * v.x + v.y * v.y);
+                        double normalized = Math.Min(1.0, magnitude / maxVelocity);
+                        byte red = (byte)(normalized * 255);
+                        */
+                        Color c = dye[x, y];
+
+                        int index = x * 4;
+                        row[index + 0] = c.B;       // Blue
+                        row[index + 1] = c.G;       // Green
+                        row[index + 2] = c.R;     // Red
+                        row[index + 3] = 255;     // Alpha
+                    }
+                }
+            }
+
+            bitmap.UnlockBits(data);
+            pictureBox1.Image = bitmap;
+            Program.UpdateSimulation(32.0f/1000);
         }
     }
 }
--- 2DFluidSim/Program.cs
+++ 2DFluidSim/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,7 +18,184 @@ namespace _2DFluidSim
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Form1 form = new Form1();
+            Application.Run(form);
+        }
+
+        public struct Vector
+        {
+            public double x;
+            public double y;
+            public Vector(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public static Vector operator +(Vector a, Vector b)
+            {
+                return new Vector(a.x + b.x, a.y + b.y);
+            }
+            public static Vector operator -(Vector a, Vector b)
+            {
+                return new Vector(a.x - b.x, a.y - b.y);
+            }
+            public static Vector operator *(Vector a, double b)
+            {
+                return new Vector(a.x*b, a.y * b);
+            }
+        }
+
+        static Vector[,] velocities = new Vector[400, 400];
+        static Color[,] dye = new Color[400, 400];
+        public static Vector[,] GetVelocities()
+        {
+            return velocities;
+        }
+        public static Color[,] GetDye()
+        {
+            return dye;
+        }
+        public static void UpdateSimulation(double t)
+        {
+            //ApplyGravity(t);
+            Advection(t);
+        }
+        public static void InitialiseVelocities()
+        {
+            for(int i = 0; i < 400; i++)
+            {
+                for(int j = 0; j < 400; j++)
+                {
+                    velocities[i,j] = new Vector(0,0);
+                }
+            }
+        }
+        public static void InitialiseDye()
+        {
+            for (int i = 0; i < 400; i++)
+            {
+                for (int j = 0; j < 400; j++)
+                {
+                    dye[i, j] = Color.Blue;
+                }
+            }
+        }
+        public static void InjectDye(int x, int y)
+        {
+            for (int i = x - 8; i < x + 8; i++)
+            {
+                for (int j = y - 8; j < y + 8; j++)
+                {
+                    if (i > 0 && j > 0 && i < 400 && j < 400)
+                    {
+                        dye[i, j] = Color.Red;
+                    }
+                }
+            }
+        }
+        public static Vector mousePos1 = new Vector(double.NaN, double.NaN);
+        public static Vector mousePos2 = new Vector(double.NaN, double.NaN);
+        public static void ApplyMouseAcceleration(int x, int y)
+        {
+            Vector currentPos = new Vector(x, y);
+
+            if (double.IsNaN(mousePos2.x))
+            {
+                mousePos2 = currentPos;
+                return;
+            }
+
+            double deltaTime = 0.032;
+            Vector velocity = new Vector(
+                (currentPos.x - mousePos2.x) / deltaTime,
+                (currentPos.y - mousePos2.y) / deltaTime
+            );
+
+            int radius = 5;
+            double strength = 0.5;
+            for (int i = x - radius; i <= x + radius; i++)
+            {
+                for (int j = y - radius; j <= y + radius; j++)
+                {
+                    if (i >= 0 && j >= 0 && i < velocities.GetLength(0) && j < velocities.GetLength(1))
+                    {
+                        double dx = i - x;
+                        double dy = j - y;
+                        double dist = Math.Sqrt(dx * dx + dy * dy);
+
+                        if (dist <= radius)
+                        {
+                            double weight = 1.0 - (dist / radius);
+                            velocities[i, j] = new Vector(
+                                velocities[i, j].x + velocity.x * weight * strength,
+                                velocities[i, j].y + velocity.y * weight * strength
+                            );
+                        }
+                    }
+                }
+                mousePos2 = currentPos;
+            }
+        }
+        static void Advection(double t)
+        {
+            Vector[,] newVelocities = new Vector[400, 400];
+            Color[,] newDye = new Color[400, 400];
+
+            Parallel.For(0, 400, y =>
+            {
+                for (int x = 0; x < 400; x++)
+                {
+                    Vector v = velocities[x, y];
+                    Vector pos = new Vector(x, y) - new Vector(v.x * t, v.y * t);
+                    (newVelocities[x, y], newDye[x, y]) = SamplePropertiesAtPos(pos);
+                    if (double.IsNaN(newVelocities[x, y].x))
+                    {
+                        newVelocities[x, y] = new Vector(-velocities[x, y].x, -velocities[x, y].y);
+                        newDye[x, y] = dye[x, y];
+                    }
+                }
+            });
+
+            // After parallel processing, copy results back to main arrays
+            Parallel.For(0, 400, y =>
+            {
+                for (int x = 0; x < 400; x++)
+                {
+                    velocities[x, y] = newVelocities[x, y];
+                    dye[x, y] = newDye[x, y];
+                }
+            });
+        }
+        static (Vector, Color) SamplePropertiesAtPos(Vector pos)
+        {
+            int x = Convert.ToInt32(Math.Floor(pos.x));
+            int y = Convert.ToInt32(Math.Floor(pos.y));
+            Vector vResult = new Vector(0, 0);
+            (int, int, int) cResult = (0, 0, 0);
+            int samples = 0;
+            for(int i = x-5; i < x+5; i++)
+            {
+                for(int j = y-5; j < y+5; j++)
+                {
+                    if(i > 0 && j > 0 && i < 400 && j < 400)
+                    {
+                        vResult += velocities[i, j];
+                        cResult = (dye[i, j].R + cResult.Item1, dye[i,j].G + cResult.Item2, dye[i,j].B + cResult.Item3);
+                        samples++;
+                    }
+                }
+            }
+            if (samples != 0)
+            {
+                vResult = new Vector(vResult.x / samples, vResult.y / samples);
+                Color color = Color.FromArgb(0, cResult.Item1 / samples, cResult.Item2 / samples, cResult.Item3 / samples);
+                return (vResult, color);
+            }
+            else
+            {
+                return (new Vector(double.NaN, double.NaN), Color.FromArgb(0,0,0,0));
+            }
         }
     }
 }
